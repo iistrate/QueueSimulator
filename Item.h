44c8f0a7bed389b02b04800ddd23e33928cfdb1a
Ioan George Istrate <istrate_ioan_george@yahoo.com>
2014-09-14 13:15:55 -0400

    Item class finished

--- Item.h
+++ Item.h
@@ -1,8 +1,23 @@
 #ifndef ITEM_H
 #define ITEM_H
 
-class Item {
+enum items {
+	TOOTHBRUSH,
+	SOAP,
+	PORK_CHOPS,
+	WATER_12oz
+};
 
+class Item {
+private:
+	const int m_name;
+public:
+	Item(const int name) : m_name(name) {}
+
+	//getters
+	const int getItemName() {
+		return m_name;
+	}
 };
 
 #endif
