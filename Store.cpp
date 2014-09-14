47f6b3bbe1e0a018aa30cde346b15f58f8d18db8
Ioan George Istrate <istrate_ioan_george@yahoo.com>
2014-09-14 17:27:32 -0400

    while in store; rand added; queues/cashiers added; counters and debug mode added

--- Store.cpp
+++ Store.cpp
@@ -1,17 +1,53 @@
 #include "Store.h"
 
 void Store::operate() {
+	//rand seed
+	srand(time(0));
+	int cashierSpeed = rand() % 9 + 1;
+	int readyToCheckout = 0;
+
+	switch (m_config) {
+	//one cashier one queue
+	case ONE_CASHIER:
+		m_Queues.push_back(new Queue);
+		break;
+	case MULTIPLE_CASHIERS_MULTIPLE_LINES:
+		//cashiers
+		//assign random speed to cashier since we won't have an actual implementation for it, but maybe later we will;
+		m_Cashiers.push_back(new Cashier(cashierSpeed));
+		m_Cashiers.push_back(new Cashier(cashierSpeed));
+		m_Cashiers.push_back(new Cashier(cashierSpeed));
+		//queues
+		m_Queues.push_back(new Queue);
+		m_Queues.push_back(new Queue);
+		m_Queues.push_back(new Queue);
+		break;
+	case MULTIPLE_CASHIERS_ONE_LINE:
+		m_Cashiers.push_back(new Cashier(cashierSpeed));
+		m_Cashiers.push_back(new Cashier(cashierSpeed));
+		m_Cashiers.push_back(new Cashier(cashierSpeed));
+		m_Queues.push_back(new Queue);
+		break;
+	}
 
 
-	Queue QueueSLine;
-	
-	QueueSLine.insert("Ioan");
-	QueueSLine.insert("Aura");
-	QueueSLine.insert("Banel");
-	QueueSLine.remove();
-	QueueSLine.printQueue();
-
+	//while in the store
 	do {
-		
+		//each tick add from 0 to 2 customers
+		readyToCheckout = rand() % 2;
+
 	} while (m_running);
+
+	/*
+		* Debug
+	*/
+	if (m_debug) {
+		Queue QueueSLine;
+
+		QueueSLine.insert("Ioan");
+		QueueSLine.insert("Aura");
+		QueueSLine.insert("Banel");
+		QueueSLine.remove();
+		QueueSLine.printQueue();
+	}
 }
--- Store.h
+++ Store.h
@@ -2,6 +2,10 @@
 #define STORE_H
 
 #include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+
 #include "Queue.h"
 #include "Cashier.h"
 
@@ -15,12 +19,14 @@ class Store {
 private:
 	bool m_running;
 	const int m_config;
+	bool m_debug;
+	int m_customers;
 
 	std::vector < Cashier* > m_Cashiers;
 	std::vector < Queue* > m_Queues;
 public:
-	Store(void) : m_running(false), m_config(Config::ONE_CASHIER) {}
-	Store(const int config) : m_running(false), m_config(config) {}
+	Store(void) : m_running(false), m_config(Config::ONE_CASHIER), m_debug(false) {}
+	Store(const int config) : m_running(false), m_config(config), m_debug(false) {}
 
 	//contains loop
 	void operate(void);
